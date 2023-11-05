d0d1f67f8f8da057a5b8ef9781de9671a5754099
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-11-05 20:24:06 +0100

    Added login and register view

--- src/app/todo/services/todo.service.ts
+++ src/app/todo/services/todo.service.ts
@@ -1,7 +1,7 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Item } from '../interfaces/item.interface';
-import { BehaviorSubject, Observable, interval, map } from 'rxjs';
+import { BehaviorSubject, Observable, catchError, interval, map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -17,6 +17,7 @@ export class TodoService {
 
   constructor(private http: HttpClient) {
     this.fetchItems();
+    console.table(this.itemsSubject);
   }
 
   getItems(): Observable<Item[]> {
@@ -28,24 +29,29 @@ export class TodoService {
   }
 
   addItem(data: Item): Observable<Item> {
-    return this.http.post<Item>(this.apiUrl, data).pipe(
-      map((item) => {
-        const currentItems = this.itemsSubject.value;
-        currentItems.push(item);
-        this.itemsSubject.next(currentItems);
-        return item;
-      })
-    );
+    return this.http.post<Item>(this.apiUrl, data)
+      .pipe(
+        map((item: Item) => {
+          const currentItems = this.itemsSubject.value;
+          currentItems.push(item);
+          this.itemsSubject.next(currentItems);
+          return item;
+        }),
+        catchError(() => {
+          throw new Error('Ocurrió un error al agregar el elemento');
+        })
+      );
   }
 
   deleteItem(id: number): Observable<unknown> {
-    return this.http.delete<Item>(`${this.apiUrl}/${id}`).pipe(
-      map(() => {
-        const currentItems = this.itemsSubject.value;
-        const updatedItems = currentItems.filter((item) => item.id_item !== id);
-        this.itemsSubject.next(updatedItems);
-      })
-    );
+    return this.http.delete<Item>(`${this.apiUrl}/${id}`)
+      .pipe(
+        map(() => {
+          const currentItems = this.itemsSubject.value;
+          const updatedItems = currentItems.filter((item) => item.id_item !== id);
+          this.itemsSubject.next(updatedItems);
+        })
+      );
   }
   // Método para cargar la lista de elementos al inicializar el servicio
   private fetchItems() {
