aed8116b010ba43ffc1bb8be9ee804de36272e3a
Alvaro Puente <alvaropuentecorrea@gmail.com>
2023-12-01 18:53:56 +0100

    Fix url

--- src/app/services/todo.service.ts
+++ src/app/services/todo.service.ts
@@ -7,16 +7,12 @@ import { BehaviorSubject, Observable, catchError, interval, map } from 'rxjs';
   providedIn: 'root',
 })
 export class TodoService {
-
   private readonly http = inject(HttpClient);
 
-  private apiUrl =
-    'https://todo-backend-springboot-production.up.railway.app/api/todoitems';
+  private apiUrl = 'https://todo-backend-springboot-production.up.railway.app/api/todoitems';
   // private apiUrl: string = 'https://todo-backend-expressjs.vercel.app/api/items'
   // private apiUrl: string = 'http://localhost:8080/api/todoitems'
-  private itemsSubject: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>(
-    []
-  );
+  private itemsSubject: BehaviorSubject<Item[]> = new BehaviorSubject<Item[]>([]);
 
   constructor() {
     this.fetchItems();
@@ -31,33 +27,31 @@ export class TodoService {
   }
 
   addItem(data: Item): Observable<Item> {
-    return this.http.post<Item>(this.apiUrl, data)
-      .pipe(
-        map((item: Item) => {
-          const currentItems = this.itemsSubject.value;
-          currentItems.push(item);
-          this.itemsSubject.next(currentItems);
-          return item;
-        }),
-        catchError(() => {
-          throw new Error('Ocurrió un error al agregar el elemento');
-        })
-      );
+    return this.http.post<Item>(this.apiUrl, data).pipe(
+      map((item: Item) => {
+        const currentItems = this.itemsSubject.value;
+        currentItems.push(item);
+        this.itemsSubject.next(currentItems);
+        return item;
+      }),
+      catchError(() => {
+        throw new Error('Ocurrió un error al agregar el elemento');
+      })
+    );
   }
 
   deleteItem(id: number): Observable<unknown> {
-    return this.http.delete<Item>(`${this.apiUrl}/${id}`)
-      .pipe(
-        map(() => {
-          const currentItems = this.itemsSubject.value;
-          const updatedItems = currentItems.filter((item) => item.id_item !== id);
-          this.itemsSubject.next(updatedItems);
-        })
-      );
+    return this.http.delete<Item>(`${this.apiUrl}/${id}`).pipe(
+      map(() => {
+        const currentItems = this.itemsSubject.value;
+        const updatedItems = currentItems.filter(item => item.id_item !== id);
+        this.itemsSubject.next(updatedItems);
+      })
+    );
   }
   // Método para cargar la lista de elementos al inicializar el servicio
   private fetchItems() {
-    this.http.get<Item[]>(this.apiUrl).subscribe((items) => {
+    this.http.get<Item[]>(this.apiUrl).subscribe(items => {
       this.itemsSubject.next(items);
       interval(30000).subscribe(() => this.fetchItems());
     });
