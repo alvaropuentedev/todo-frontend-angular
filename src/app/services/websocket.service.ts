68682c672109becf887e206833152f7f18a4825e
Alvaro Puente <alvaropuentedev@gmail.com>
2025-04-07 11:15:43 +0200

    fix: websocket auto reconnect

--- src/app/services/websocket.service.ts
+++ src/app/services/websocket.service.ts
@@ -1,25 +1,69 @@
-
-import { Injectable } from '@angular/core';
-import { Observable } from 'rxjs';
+import { Injectable, OnDestroy } from '@angular/core';
+import { Observable, Subject, timer, Subscription } from 'rxjs';
 import { webSocket, WebSocketSubject } from 'rxjs/webSocket';
-import { enviroment } from "../../environments/environments";
+import { enviroment } from '../../environments/environments';
 
 @Injectable({
   providedIn: 'root'
 })
-export class WebSocketService {
+export class WebSocketService implements OnDestroy {
+  private socket$: WebSocketSubject<any> | undefined;
+  private connection$: Subject<any> = new Subject();
+  private reconnectDelay = 3000;
+  private reconnectSubscription?: Subscription;
+
   private readonly webSocketUrl: string = enviroment.websocket_url;
-  private socket$: WebSocketSubject<any>;
 
   constructor() {
-    this.socket$ = webSocket(this.webSocketUrl);
+    this.connect();
+  }
+
+  private connect(): void {
+    this.socket$ = webSocket({
+      url: this.webSocketUrl,
+      openObserver: {
+        next: () => {
+          console.log('✅ WebSocket conectado');
+        }
+      },
+      closeObserver: {
+        next: () => {
+          console.log('❌ WebSocket cerrado, reintentando...');
+          this.reconnect();
+        }
+      }
+    });
+
+    this.socket$.subscribe({
+      next: (msg) => this.connection$.next(msg),
+      error: (err) => {
+        console.error('WebSocket error:', err);
+        this.reconnect();
+      },
+      complete: () => {
+        console.log('WebSocket completado, reconectando...');
+        this.reconnect();
+      }
+    });
+  }
+
+  private reconnect(): void {
+    if (this.reconnectSubscription) {
+      this.reconnectSubscription.unsubscribe();
+    }
+    this.reconnectSubscription = timer(this.reconnectDelay).subscribe(() => this.connect());
   }
 
   public sendMessage(message: any): void {
-    this.socket$.next(message);
+    this.socket$?.next(message);
   }
 
   public getMessages(): Observable<any> {
-    return this.socket$.asObservable();
+    return this.connection$.asObservable();
+  }
+
+  ngOnDestroy(): void {
+    this.socket$?.complete();
+    this.reconnectSubscription?.unsubscribe();
   }
 }
